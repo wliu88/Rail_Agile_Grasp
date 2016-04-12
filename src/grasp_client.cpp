7723724cf9296f74bf1b660dc3704a4167898fdd
wliu88 <weiyuliu69@yahoo.com>
2016-04-12 10:37:57 -0400

    use msg and action

--- src/grasp_client.cpp
+++ src/grasp_client.cpp
@@ -5,18 +5,30 @@
 #include <rail_manipulation_msgs/SegmentedObjectList.h>
 #include <tf/transform_listener.h>
 #include <std_srvs/Empty.h>
+#include <rail_agile_grasp_msgs/Workspace.h>
+#include <rail_agile_grasp_msgs/FindGraspsAction.h>
 
+// message callback after segmentation service is called
 void objects_callback(const rail_manipulation_msgs::SegmentedObjectList &objectList);
+
 //sensor_msgs::PointCloud2 cloud_stored;
+
+// a list of workspaces of cluttered objects
+std::list<rail_agile_grasp_msgs::Workspace> workspaceList;
 
 int main (int argc, char **argv)
 {
 	ros::init(argc, argv, "grasp");
- 	ros::NodeHandle nh;
+	ros::NodeHandle nh;
 	ros::Subscriber sub = nh.subscribe("/object_recognition_listener/recognized_objects", 100, objects_callback);
+	// initialize a service client for segmentation
 	ros::ServiceClient segmentClient = nh.serviceClient<std_srvs::Empty>("/rail_segmentation/segment");
+	// initialize a action client for finding grasps
+	actionlib::SimpleActionClient<rail_agile_grasp_msgs::FindGraspsAction> findGraspsClient("/agile_grasp/find_grasps", true);
+	//findGraspsClient.waitForServer();
+
 	//ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("segmented_cloud", 1000);
-    
+
 	/***
   	// create the action client
   	// true causes the client to spin its own thread
@@ -79,30 +91,40 @@ int main (int argc, char **argv)
 
 	ros::Rate rate(10.0);
 
-	int i = 0;
+	bool calledSegmentation = false;
 	while(nh.ok())
 	{
 		ros::spinOnce();
-		if (i == 0) {
+		if(!calledSegmentation) {
 			std_srvs::Empty segmentSrv;
 			if (!segmentClient.call(segmentSrv))
-    		{
-    			ROS_INFO("Couldn't call segmentation service.");
-    		}
-    		else 
-    		{
+			{
+				ROS_INFO("Couldn't call segmentation service.");
+			}
+			else 
+			{
 				ROS_INFO("Called segmentation service.");
-    			i = 1;
-    		}
-    	}
-    	rate.sleep();
+				calledSegmentation = true;
+			}
+		}
+
+		if (!workspaceList.empty())
+		{
+			//nh.deleteParam("/find_grasps/workspace");
+			//nh.setParam("/find_grasps/workspace", workspaceList.front());
+			rail_agile_grasp_msgs::FindGraspsGoal findGraspsGoal;
+			findGraspsGoal.workspace = workspaceList.front();
+			workspaceList.pop_front();
+			findGraspsClient.sendGoal(findGraspsGoal);
+			//findGraspsClient.waitForResult(ros::Duration(10));
+			ROS_INFO("send one goal");
+			// wait for the action to complete
+		}
+		rate.sleep();
 	}
 
-  	return 0;
+	return 0;
 }
-
-
-
 
 
 void objects_callback(const rail_manipulation_msgs::SegmentedObjectList &objectList)
@@ -112,22 +134,22 @@ void objects_callback(const rail_manipulation_msgs::SegmentedObjectList &objectL
 	for (int i = 0; i < objectList.objects.size(); i++)
 	{	
 		rail_manipulation_msgs::SegmentedObject object = objectList.objects[i];
-		ROS_INFO("This is object No.%d\n", i);
-		ROS_INFO("This object is recognized? %d\n", object.recognized);
-		ROS_INFO("The center of the object is\n\tx: %f\n\ty: %f\n\tz: %f\n", 
-				 object.center.x, object.center.y, object.center.z);
-
 		
-		geometry_msgs::PointStamped pt;
-		geometry_msgs::PointStamped pt_transformed;
-		pt.point = object.center;
-
+		//ROS_INFO("This is object No.%d\n", i);
+		//ROS_INFO("This object is recognized? %d\n", object.recognized);
+		//ROS_INFO("The center of the object is\n\tx: %f\n\ty: %f\n\tz: %f\n", 
+		//	object.center.x, object.center.y, object.center.z);
+		
 		tf::TransformListener listener;
 		const ros::Time time = ros::Time(0);
 		const std::string target_frame = "kinect2_rgb_optical_frame";
 		const std::string reference_frame = "table_base_link";
+		geometry_msgs::PointStamped pt;
+		geometry_msgs::PointStamped pt_transformed;
+		pt.point = object.center;
 		pt.header.frame_id = reference_frame;
-		ros::Rate rate(10.0);
+		
+		ros::Rate rate(100.0);
 		while(true)
 		{
 			try
@@ -144,9 +166,19 @@ void objects_callback(const rail_manipulation_msgs::SegmentedObjectList &objectL
 		}
 		ROS_INFO("Find transform");
 
-
 		ROS_INFO("The center of the object after transform is\n\tx: %f\n\ty: %f\n\tz: %f\n", 
-				 pt_transformed.point.x, pt_transformed.point.y, pt_transformed.point.z);
+			     pt_transformed.point.x, pt_transformed.point.y, pt_transformed.point.z);
+		rail_agile_grasp_msgs::Workspace wp;
+		wp.x_min = pt_transformed.point.x - 0.1;
+		wp.x_max = pt_transformed.point.x + 0.1;
+		wp.y_min = pt_transformed.point.y - 0.1;
+		wp.y_max = pt_transformed.point.y + 0.1;
+		wp.z_min = -10;
+		wp.x_max = 10;
+		workspaceList.push_back(wp);
+
+		 //{pt_transformed.point.x-0.1, pt_transformed.point.x+0.1,
+		//							  pt_transformed.point.y-0.1, pt_transformed.point.y+0.1, -10, 10};
 		//ROS_INFO("The dimension of the object is\n\twidth: %f\n\tdepth: %f\n\theight: %f\n\n", object.width, object.depth, object.height);
 		
 	}
